0e03872753e1a1de076e84414f476a9d40a8ffd3
张雨 <likesnooker@zhangyudeMacBook-Pro.local>
2018-12-23 08:54:53 +0800

    第四题使用动态规划求解

--- subject-4.cpp
+++ subject-4.cpp
@@ -3,40 +3,39 @@
 class Solution {
 public:
     /**
-     * @param n, An integer
-     * @return: the nth prime number as description.
+     * param n, An integer
+     * return: the nth prime number as description.
      */
-    bool ugly(int num)
-    {
-        if (num == 1)
-            return true;
-        if(num == 2)
-            return true;
-        if(num == 3)
-            return true;
-        if(num == 5)
-            return true;
-        if( (num & 1) == 0)
-            return ugly(num >> 1);
-        if(num % 3 == 0)
-            return ugly(num /3);
-        if(num % 5 == 0)
-            return ugly(num /5);
-        return false;
-    }
+    // 用动态规划的思路求解
+    // 考虑到一个ugly number可以通过对另一个ugly number乘上2、3、5获得,我们可以维系一个保存了
+    // ugly  number 的队列,将队列中的某个数乘上2、3、5 并添加到队列中
     int nthUglyNumber(int n) {
-        int num = 1;
-        int i   = 0;
-        while (i < n) {
-            if(ugly(num))
-            {
-                i++;
-                num++;
-            }
-            else
-                num ++;
+        vector<int> ugly_nums;
+        ugly_nums.push_back(1);
+        
+        int num2_ix = 0;
+        int num3_ix = 0;
+        int num5_ix = 0;
+        
+        int nth = 1;
+        while(nth < n)
+        {
+            int num2 = ugly_nums[num2_ix] * 2;
+            int num3 = ugly_nums[num3_ix] * 3;
+            int num5 = ugly_nums[num5_ix] * 5;
+            int temp  = num2 < num3 ? num2 : num3;
+            int min   = temp < num5 ? temp : num5;
+            ugly_nums.push_back(min);
+            
+            if(ugly_nums[num2_ix] * 2 <= ugly_nums.back())
+                num2_ix ++;
+            if(ugly_nums[num3_ix] * 3 <= ugly_nums.back())
+                num3_ix ++;
+            if(ugly_nums[num5_ix] * 5 <= ugly_nums.back())
+                num5_ix ++;
+            
+            nth ++;
         }
-        // write your code here
-        return num-1;
+        return ugly_nums.back();
     }
 };
